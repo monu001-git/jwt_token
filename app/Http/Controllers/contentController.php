4d63ad2e60e9cea9588a2f50688320e17bd25c1d
monu tyagi <monutyagi996@gmail.com>
2024-10-01 10:27:05 +0530

    fixed

--- app/Http/Controllers/contentController.php
+++ app/Http/Controllers/contentController.php
@@ -7,28 +7,36 @@ use App\Models\content;
 
 class contentController extends Controller
 {
-   
 
     public function addContent(Request $request)
     {
-    //   $files = $request->imageFiles;  
-      $titles = $request->imageTitles; 
-      $alts = $request->imageAlts;  
+        $request->validate([
+            'titles.*' => 'required|string|max:255',
+            'alts.*' => 'required|string|max:255',
+            'files.*' => 'file|mimes:jpg,jpeg,png,pdf|max:2048',
+        ]);
 
-        foreach ($titles as $index => $title) {
+        $titles = $request->input('titles');
+        $alts = $request->input('alts');
+        $files = $request->file('files');
+
+        foreach ($files as $index => $file) {
             $data = new Content();
             $data->title = $titles[$index];
             $data->alt = $alts[$index];
+
+            $path = public_path('content');
+            if ($file) {
+                $newname = time() . rand(10, 99) . '.' . $file->getClientOriginalExtension();
+                $file->move($path, $newname);
+                $data->file = $newname;
+            }
             $data->save();
         }
 
         return response()->json([
             'status' => 200,
-            'message' => 'Record added successfully',
+            'message' => 'Records added successfully',
         ]);
     }
-
-    
-    
-
 }
