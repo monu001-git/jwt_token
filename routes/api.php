076f30bd840a76ccd9aff795a8e7ae3dbd97a78e
monu tyagi <monutyagi996@gmail.com>
2024-10-16 10:30:32 +0530

    fixed

--- routes/api.php
+++ routes/api.php
@@ -15,64 +15,67 @@ use App\Http\Controllers\orgStructuteController;
 use App\Http\Controllers\forgetPasswordController;
 
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    // Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-});
-
-Route::controller(userController::class)->group(function () {
-    Route::get('user', 'viewUser');
-    Route::post('add-edit-user', 'addUser');
-    Route::delete('delete-user', 'deleteUser');
-});
+Route::middleware('auth:api')->group(function () {
 
 
-Route::controller(profileController::class)->group(function () {
-    Route::get('get-profile', 'getProfile');
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('refresh', 'refresh');
+    });
+
+    Route::controller(userController::class)->group(function () {
+        Route::get('user', 'viewUser');
+        Route::post('add-edit-user', 'addUser');
+        Route::delete('delete-user', 'deleteUser');
+    });
+
+    Route::controller(profileController::class)->group(function () {
+        Route::get('get-profile', 'getProfile');
+    });
+
+    Route::controller(bannerController::class)->group(function () {
+        Route::get('get-banner', 'getBanner');
+    });
+
+    Route::controller(menuController::class)->group(function () {
+        Route::get('menu', 'getMenu');
+        Route::post('add-edit-menu', 'addMenu');
+        Route::delete('delete-menu', 'deleteMenu');
+        Route::get('parentMaster', 'parentMaster');
+        Route::get('menu-tree', 'buildMenuTree');
+    });
+
+    Route::controller(contentController::class)->group(function () {
+        Route::get('content', 'getContent');
+        Route::post('add-edit-content', 'addContent');
+        Route::delete('delete-content', 'deleteContent');
+    });
+
+    Route::controller(orgStructuteController::class)->group(function () {
+        Route::get('org', 'getOrg');
+        Route::post('add-edit-org', 'addOrg');
+        Route::delete('delete-org', 'deleteOrg');
+    });
+
+    Route::controller(commanController::class)->group(function () {
+        Route::get('status-change/{status}/{id}/{db}', 'StatusChange');
+    });
+
+    Route::post('send-mail', [MailController::class, 'index']);
 });
 
-Route::controller(bannerController::class)->group(function () {
-    Route::get('get-banner', 'getBanner');
-});
-
-Route::controller(menuController::class)->group(function () {
-    Route::get('menu', 'getMenu');
-    Route::post('add-edit-menu', 'addMenu');
-    Route::delete('delete-menu', 'deleteMenu');
-    Route::get('parentMaster', 'parentMaster');
-    Route::get('menu-tree', 'buildMenuTree');
-});
-
-Route::controller(contentController::class)->group(function () {
-    Route::get('content', 'getContent');
-    Route::post('add-edit-content', 'addContent');
-    Route::delete('delete-content', 'deleteContent');
-});
-
-Route::controller(orgStructuteController::class)->group(function () {
-    Route::get('org', 'getOrg');
-    Route::post('add-edit-org', 'addOrg');
-    Route::delete('delete-org', 'deleteOrg');
-});
-
-Route::controller(commanController::class)->group(function () {
-    Route::get('status-change/{status}/{id}/{db}', 'StatusChange');
-});
 
 Route::controller(homeController::class)->group(function () {
     Route::get('headerMenu', 'headerMenu');
     Route::get('footerMenu', 'footerMenu');
 });
 
-Route::controller(forgetPasswordController::class)->group(function () {
- 
-Route::post('forget-password','submitForgetPasswordForm')->name('forget.password.post'); 
-Route::post('reset-password', 'submitResetPasswordForm')->name('reset.password.post');
-
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
 });
 
-
-
-Route::post('send-mail', [MailController::class, 'index']);
+Route::controller(forgetPasswordController::class)->group(function () {
+    Route::post('forget-password', 'submitForgetPasswordForm')->name('forget.password.post');
+    Route::post('reset-password', 'submitResetPasswordForm')->name('reset.password.post');
+});
